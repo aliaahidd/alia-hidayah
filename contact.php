bc4b694e995903b40c8aeb9d6ec863aa18d146c3
Alia Hidayah <56750777+aliaahidd@users.noreply.github.com>
2021-08-30 21:17:48 +0800

    Update contact.php

--- contact.php
+++ contact.php
@@ -2,7 +2,7 @@
 if (isset($_POST['Email'])) {
 
     // EDIT THE 2 LINES BELOW AS REQUIRED
-    $email_to = "aliahidayah00@gmail.com";
+    $email_to = "aliahidayah14@yahoo.com";
     $email_subject = "New form submissions";
 
     function problem($error)
